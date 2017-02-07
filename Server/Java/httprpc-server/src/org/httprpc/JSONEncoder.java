d6b76a10607aed93b6907128a20d0035a120b697
crabo <kitwall@163.com>
2017-02-07 09:54:58 +0800

    bugfix: Direct ouput JSON string without encode

--- Server/Java/httprpc-server/src/org/httprpc/JSONEncoder.java
+++ Server/Java/httprpc-server/src/org/httprpc/JSONEncoder.java
@@ -45,6 +45,17 @@ public class JSONEncoder implements Encoder {
     @Override
     public void writeValue(Object value, OutputStream outputStream) throws IOException {
         Writer writer = new OutputStreamWriter(outputStream, Charset.forName(UTF_8_ENCODING));
+        //by crabo: render JSON string directly!
+        if ((value != null) && ((value instanceof CharSequence)))
+        {
+          CharSequence string = (CharSequence)value;
+          if ((string.charAt(0) == '[') || (string.charAt(0) == '{'))
+          {
+            writer.append(string);
+            writer.flush();
+            return;
+          }
+        }
         writeValue(value, writer);
 
         writer.flush();
